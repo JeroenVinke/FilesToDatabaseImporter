8457b4a431d112345ade628480e0aa6325463838
JeroenVinke <Jeroenvinke17@gmail.com>
2014-10-20 18:27:03 +0200

    Added IDispatcher

---
+++ FilesToDatabaseImporter/Interfaces/IDispatcher.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace FilesToDatabaseImporter.Interfaces
+{
+    public interface IDispatcher
+    {
+        void Invoke(Action action);
+        void BeginInvoke(Action action);
+    }
+}
